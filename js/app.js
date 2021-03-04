d28a30fec8023680a2bab036638658784f2b6574
linusdinesjo <73996858+linusdinesjo@users.noreply.github.com>
2021-03-04 22:15:21 +0100

    Add files via upload
    Tog bort about. 
    
    Algoritmer går nu längs diagonal mellan start och mål för mer "mänsklig" eller "realistisk" avbildning av den kortaste vägen.
    
    Clear-knappen laddar inte om sidan, utan återställer alla noder istället.
    
    Små estetiska ändringar i tabellens celler.
    
    För två uppdateringar sen kan man btw rita istället för att klicka ut hinder.
    
    Ändrade "euclidean"-avståndsfunktionen till diagonalt avstånd, taget från http://theory.stanford.edu/~amitp/GameProgramming/Heuristics.html .

--- js/app.js
+++ js/app.js
@@ -3,6 +3,7 @@ var startNode;
 var endNode;
 var startExists = false;
 var endExists = false;
+var hasBeenCleared = true;
 
 var strokeID = 0;
 var erasing = false;
@@ -38,6 +39,29 @@ document.addEventListener("mouseup", () => {
   document.removeEventListener("mousemove", handleMousemove);
 });
 
+function handleClearButton() {
+  nodes.forEach(nodeRow => {
+    nodeRow.forEach(node => {
+      node.type = "normal";
+      node.fDistance = Infinity;
+      node.gDistance = Infinity;
+      node.hDistance = Infinity;
+      node.nbrs = [];
+      node.usedStrokeIDs = [];
+      node.clearClasses();
+    });
+  });
+  startExists = false;
+  endExists = false;
+  startNode = undefined;
+  endNode = undefined;
+  hasBeenCleared = true;
+}
+
+function handleRunButton() {
+  runPathfinding(nodes);
+}
+
 
 class Node {
   constructor(x, y, type) {
@@ -56,6 +80,12 @@ class Node {
 
   addClass(c) {
     document.getElementsByClassName(`_${this.x}-${this.y}`)[0].classList.add(c);
+  }
+
+  clearClasses() {
+    while (document.getElementsByClassName(`_${this.x}-${this.y}`)[0].classList.length > 1) {
+      document.getElementsByClassName(`_${this.x}-${this.y}`)[0].classList.remove(document.getElementsByClassName(`_${this.x}-${this.y}`)[0].classList[1]);
+    }
   }
 
   makeStart() {
@@ -134,6 +164,7 @@ var id;
 var intervalTime = 0; // time between each iteration in milliseconds, 0 = real time
 
 function runPathfinding(nodes) {
+  hasBeenCleared = false;
   // Make sure start and end exist
   if (startNode == undefined || endNode == undefined) {
     alert("Select a start and an end node!");
@@ -181,7 +212,7 @@ function runDijkstraOnce() {
 
       // fDistance represents distance to start in order to
       // still use getLowestDistance function
-      provisionalfDistance = curNode.fDistance + getEuclideanDistance(curNode, nbr);
+      provisionalfDistance = curNode.fDistance + getEuclideanDistance(curNode, nbr) + getCross(nbr) * 0.001;
       if (!(openSet.includes(nbr)) || (provisionalfDistance < nbr.fDistance)) {
         nbr.cameFrom = curNode;
         nbr.fDistance = provisionalfDistance;
@@ -214,12 +245,12 @@ function runAstarOnce() {
   // Update each nbrs distance
   curNode.nbrs.forEach(nbr => {
     if (!closedSet.includes(nbr)) { // if in consideration
-      
+
       let provisionalgDistance = curNode.gDistance + getEuclideanDistance(curNode, nbr);
       if (!(openSet.includes(nbr)) || provisionalgDistance < nbr.gDistance) {
         nbr.cameFrom = curNode;
         nbr.gDistance = provisionalgDistance;
-        nbr.hDistance = getDistance(nbr, endNode);
+        nbr.hDistance = getEuclideanDistance(nbr, endNode) + getCross(nbr) * 0.001;
         nbr.fDistance = nbr.gDistance + nbr.hDistance;
         if (!(openSet.includes(nbr))) {
           openSet.push(nbr);
@@ -232,6 +263,14 @@ function runAstarOnce() {
   curNode.addClass("visited"); // CSS
 }
 
+function getCross(node) {
+  let dx1 = node.x - endNode.x;
+  let dy1 = node.y - endNode.y;
+  let dx2 = startNode.x - endNode.x;
+  let dy2 = startNode.y - endNode.y;
+  let cross = Math.abs(dx1 * dy2 - dx2 * dy1);
+  return cross;
+}
 
 function getLowestDistance(arr) {
   // Returns node with lowers f-cost, 
@@ -267,15 +306,19 @@ function visualiseCameFrom(latestNode) {
 
 
 function getDistance(node1, node2) {
-  // Returns distance in manhattan measurement
+  // Returns distance in Manhattan measurement
 
   return Math.abs(node1.x - node2.x) + Math.abs(node1.y - node2.y);
 }
 
 function getEuclideanDistance(node1, node2) {
-  // Returns distance in Euclidean measurement
-  
-  return Math.sqrt(Math.pow((node2.x - node1.x), 2) + Math.pow((node2.y - node1.y), 2));
+  // Returns distance in euclidean measurement
+
+  // return Math.sqrt(Math.pow((node2.x - node1.x), 2) + Math.pow((node2.y - node1.y), 2));
+  let dx = Math.abs(node1.x - node2.x);
+  let dy = Math.abs(node1.y - node2.y);
+  return (dx + dy) + (Math.sqrt(2) - 2) * Math.min(dx, dy);
+
 }
 
 
