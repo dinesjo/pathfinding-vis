b39e679dd415a7602b4a6c4346898d75ea9a17e0
dinesjo <dinesjo@kth.se>
2023-06-05 09:01:44 +0200

    Improve accessibility with colorblind mode

---
+++ js/colorblind.js
@@ -0,0 +1,39 @@
+var cb = false;
+var cbButton = document.getElementById("toggleColorblind");
+var clearObj = document.getElementById("clearObjectives");
+var greens = document.getElementsByClassName("green");
+var reds = document.getElementsByClassName("red");
+var start = document.getElementsByClassName("start");
+var end = document.getElementsByClassName("end");
+var defaultCbButtonBGColor = cbButton.style.backgroundColor;
+
+function toggleColorblind() {
+  if (!cb) {
+    // apply colorblind mode
+    cbButton.innerText = "On";
+    cbButton.style.backgroundColor = "deeppink";
+    clearObj.classList.add("cb");
+    greens[0].classList.add("cb");
+    reds[0].classList.add("cb");
+    try {
+      start[0].classList.add("cb");
+      end[0].classList.add("cb");
+    } catch (TypeError) {} // if either start or end not yet placed
+    
+    cb = true;
+  } else {
+    // Reset to default
+    cbButton.innerText = "Off";
+    cbButton.style.backgroundColor = defaultCbButtonBGColor;
+    clearObj.classList.remove("cb");
+    greens[0].classList.remove("cb");
+    reds[0].classList.remove("cb");
+    try {
+      start[0].classList.remove("cb");
+      end[0].classList.remove("cb");
+    } catch (TypeError) {} // if either start or end not yet placed
+
+    cb = false;
+  }
+}
+
